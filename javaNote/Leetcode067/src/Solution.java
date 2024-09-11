dfdb794c8afc0c4b86cdb31a40cb1de30d7c1000
Leona-Wang <leonawang920913@gmail.com>
2024-09-11 17:02:12 +0800

    2409111702

--- javaNote/Leetcode067/src/Solution.java
+++ javaNote/Leetcode067/src/Solution.java
@@ -6,11 +6,51 @@ public class Solution {
 
     }
 
-    public String addBinary(String a, String b){
+
+    //我寫的2
+    /*public String addBinary(String a, String b){
         String result="";
 
+        int max=Math.max(a.length(),b.length());
+
+        int temp=0;
+
+        char[] aToChar=new char[max];
+        char[] bToChar=new char[max];
+
+        for (int i=0;i<max-a.length();i++){
+            aToChar[i]='0';
+        }
+        for (int j=max-a.length();j<max;j++){
+            aToChar[j]=a.charAt(temp);
+            temp++;
+        }
+
+        temp=0;
+        for (int i=0;i<max-b.length();i++){
+            bToChar[i]='0';
+        }
+        for (int j=max-b.length();j<max;j++){
+            bToChar[j]=b.charAt(temp);
+            temp++;
+        }
+
+        temp=0;
+        int cal=0;
+
+        for (int i=max-1;i>=0;i--){
+
+            cal=Character.getNumericValue(aToChar[i])+Character.getNumericValue(bToChar[i])+temp;
+            result=(cal%2)+result;
+            temp=cal/2;
+        }
+
+        if (temp==1){
+            result="1"+result;
+        }
+
         return result;
-    }
+    }*/
 
     //我寫的
     /*public String addBinary(String a, String b) {
